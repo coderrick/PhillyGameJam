e959d4a6f2640c9c3075f118b2218347ef766cfb
coderrick <derrick.duncan@temple.edu>
2016-11-19 20:57:59 -0500

    Placed renderer.render in render function in order to animate it frame by frame with animationFrame.

--- App/public/javascripts/code.js
+++ App/public/javascripts/code.js
@@ -23,4 +23,11 @@ mesh.position.set(0, 0, -1000);
 
 scene.add(mesh);
 
-renderer.render(scene, camera);
+requestAnimationFrame(render);
+
+function render(){
+  mesh.rotation.x += 0.1;
+  mesh.rotation.y += 0.1;
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+}
