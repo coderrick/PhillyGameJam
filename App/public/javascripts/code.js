aa24b5f67750eb4731759d52a143371d4be49d15
coderrick <derrick.duncan@temple.edu>
2016-11-19 20:51:15 -0500

    Added hello world mesh to the scene

--- App/public/javascripts/code.js
+++ App/public/javascripts/code.js
@@ -9,8 +9,18 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 //Camera there different kinds of caremars                                        near  far for clipping
 var camera = new THREE.PerspectiveCamera(35, window.innerWidth/window.innerHeight, 0.1, 3000); 
+//camera.position.set(0, 0, 0);
 
 //Scene
 var scene = new THREE.Scene();
+
+//Geometry
+var geometry = new THREE.BoxGeometry(100, 100, 100);
+var material = new THREE.MeshBasicMaterial();
+var mesh = new THREE.Mesh(geometry, material);
+//NOTE camera position is set to (0, 0, 0) by default; geo will not be scene unless position is reset as shown below:
+mesh.position.set(0, 0, -1000);
+
+scene.add(mesh);
 
 renderer.render(scene, camera);
