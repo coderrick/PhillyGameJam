0f37ff25d2dbcccbbb31ae21c43c45f6cb086a0e
coderrick <derrick.duncan@temple.edu>
2016-11-21 23:55:34 -0500

    update

---
+++ App/public/javascripts/canvas.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById('c');
+var ctx = canvas.getContext('2d');
+ctx.font = 'italic 18px Arial';
+ctx.textAlign = 'center';
+ctx. textBaseline = 'middle';
+ctx.fillStyle = 'red'; 
+ctx.fillText('Your Text', 150, 50);
